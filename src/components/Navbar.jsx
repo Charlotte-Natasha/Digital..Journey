cc56ac30df759ffd02330c89818ffd51c0fdb649
Charlotte Natasha <natashaonyango97@gmail.com>
2022-09-08 23:31:46 +0200

    navbar responsiveness for phone and comp achieved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
+
+    const[nav, setNav] = useState(false);
 
     const links = [
         {
@@ -37,11 +40,30 @@ const Navbar = () => {
                 <h1 className='text-5xl font-signature ml-2'>Charlotte</h1>
             </div>
 
-            {links.map(({id, link}) => (
-                <ul className='flex'>
+            <ul className='hidden md:flex'>
+                {links.map(({id, link}) => (
+                
                 <li key={id} className='px-4 cursor-pointer capitalize font-medium text-pink-500 hover:scale-105 duration-200'>{link}</li>
-            </ul>
+            
             ))}
+            </ul>
+
+            <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-pink-500 md:hidden'>
+                {nav ?<FaTimes size={30}/> : <FaBars size={30}/> }
+            </div>
+
+            {nav && (
+                <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-pink-800 text-pink-500'>
+                {links.map(({id, link}) => (
+                    
+                    <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>{link}</li>
+                
+                ))}
+                    
+                </ul>
+            )}
+
+            
 
         </div>
     )
