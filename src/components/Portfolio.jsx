caad4270ebde66ae31641013dc199b5a7e602192
Charlotte Natasha <natashaonyango97@gmail.com>
2022-09-09 22:03:44 +0200

    styled portfolio buttons and image

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,11 +12,11 @@ const Portfolio = () => {
                     <p className='py-6'>Check out some of my projects</p>
                 </div>
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-                    <div>
-                        <img src={award} alt="" />
-                        <div>
-                            <button>Demo</button>
-                            <button>Code</button>
+                    <div className='shadow-md shadow-purple-600 rounded-lg'>
+                        <img src={award} alt="" className='rounded-md duration-200 hover:scale-105' />
+                        <div className='flex items-center justify-center'>
+                            <button className='w-1/2 px-6 first-letter:py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                            <button className='w-1/2 px-6 first-letter:py-3 m-4 duration-200 hover:scale-105'>Code</button>
                         </div>
                     </div>
                 </div>
