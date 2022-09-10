4cb7694e8fa02989dfffea1b17eacebc6cf90205
Charlotte Natasha <natashaonyango97@gmail.com>
2022-09-10 20:39:51 +0200

    styled the experience file

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+// import html from '../assets/html.png'
+// import css from '../assets/css.png'
+// import reactImage from '../assets/react.png'
+// import node from '../assets/node.png'
+// import javascript from '../assets/javascript.png'
+// import tailwind from '../assets/tailwind.png'
+// import github from '../assets/tailwind.png'
 
 const Experience = () => {
     return (
-        <div>
-            <div>
-                <div>
-                    <p>Experience</p>
-                    <p>These are some of the technologies that I have worked with.</p>
+        <div name='experience' className='w-full h-screen bg-gradient-to-b from-pink-900 to-black text-pink-50'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-black' >Experience</p>
+                    <p className='py-6'>These are some of the technologies that I have worked with.</p>
                 </div>
             </div>
         </div>
