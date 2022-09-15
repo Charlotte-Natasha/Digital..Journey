788584b4d3a3c8175416098dddccf6169946027e
Charlotte Natasha <natashaonyango97@gmail.com>
2022-09-15 17:27:38 +0200

    added screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     extend: {},
     fontFamily: {
       signature: ['Great Vibes']
-    }
+    },
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '992px',
+      // => @media (min-width: 992px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1200px) { ... }
+    },
   },
   plugins: [],
 }
